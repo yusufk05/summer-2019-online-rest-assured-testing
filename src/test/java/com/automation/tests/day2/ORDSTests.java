b7852b00a38149f6b862cb286d19462fec734457
yusufk05 <ykaracin@gmail.com>
2020-01-28 15:41:05 -0500

    status code test

--- src/test/java/com/automation/tests/day2/ORDSTests.java
+++ src/test/java/com/automation/tests/day2/ORDSTests.java
@@ -1,4 +1,26 @@
 package com.automation.tests.day2;
 
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
 public class ORDSTests {
+    //address of ORDS web service, that is running on AWS ec2.
+    //data is coming from SQL Oracle data base to this web service
+    //during back-end testing with SQL developer and JDBC API
+    //we were accessing data base directly
+    //now, we gonna access web service
+    private String baseURI = "http://ec2-54-85-201-7.compute-1.amazonaws.com:1000/ords/hr";
+
+    //verify that status code is 200
+    @Test
+    public void test1(){
+        Response response = given().get(baseURI+"/employees");
+
+        System.out.println(response.getBody().asString());
+        assertEquals(200,response.getStatusCode());
+    }
+
 }
